ed403c348b1c07668715385e2890027182b23188
Amber <amberrose9167@gmail.com>
2019-01-18 22:07:48 -0800

    added the hobbies to a table with togglemodal btn

--- client/src/components/meetupModal/meetupModal.js
+++ client/src/components/meetupModal/meetupModal.js
@@ -6,9 +6,9 @@ import {
     ModalBody,
     ModalFooter,
 } from 'reactstrap';
-import { Link } from "react-router-dom"
 
-const Persona = (props) =>
+
+const MeatUp = (props) =>
     <div>
         <Modal isOpen={props.modal} toggle={props.toggleModal} className={props.className}>
             <ModalHeader toggle={props.toggleModal}>{props.modalTitle}</ModalHeader>
@@ -17,12 +17,9 @@ const Persona = (props) =>
                 <h3 className="psMdl">with links to their meetup page </h3>
             </ModalBody>
             <ModalFooter>
-                <Link to="/profile">
-                    <Button className="button">To Your Profile</Button>
-                </Link>
+                <Button className="button" onClick={props.toggleModal}>Back</Button>
             </ModalFooter>
         </Modal>
     </div >
 
-export default Persona;
-
+export default MeatUp;
--- client/src/pages/profile/Profile.js
+++ client/src/pages/profile/Profile.js
@@ -1,27 +1,42 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Jumbotron, Button, ListGroup, ListGroupItem } from "reactstrap";
+import { Container, Row, Col, Jumbotron, Button, Table } from "reactstrap";
 import Hobbies from "../../Hobbies.json";
 import { Link } from "react-router-dom";
+import MeatUp from '../..//components/meetupModal';
 
+require("dotenv").config();
 
-
-const hobbys = Hobbies.hobbies
+const hobbys = Hobbies.hobbies;
 let hobbyList = [];
+
+const APIkey = process.env.API_Key;
+let hobbylink = "";
+let URL = `https://api.meetup.com/find/groups?key=${APIkey}&sign=true&photo-host=public&zip=90274&fallback_suggestions=true&text=${hobbylink}&radius=25.0&page=20`;
+
 
 class Profile extends Component {
   constructor(props) {
     super(props);
     this.state = {
       user: "",
-      hobbies: []
+      hobbies: hobbyList,
+      meetUpModal: false,
+      isOpen: false,
     };
   }
 
   componentDidMount() {
     this.getSession();
     this.getHobbies();
+    this.saveSession();
   }
 
+  saveSession = () => {
+    // Save data to sessionStorage
+    for (let key in this.state) {
+      localStorage.setItem(key, JSON.stringify(this.state[key]));
+    }
+  }
   getSession = () => {
     // Get saved data from localStorage
     for (let key in this.state) {
@@ -48,56 +63,87 @@ class Profile extends Component {
         hobbyList.push(hobby);
       }
     }
-    this.setState({
-      hobbies: hobbyList
-    })
+
     // console.log(hobbys)
     // console.log(hobbyList);
 
   }
 
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  togglemeetUpModal = () => {
+    this.setState({
+      meetUpModal: !this.state.meetUpModal
+    });
+    console.log(this)
+  }
+
   render() {
     return (
-      <Container>
-        <Row>
-          <Col>
-            <Jumbotron>
-              <h1 className="display-3">Hello, {this.props.user.first_name}</h1>
-              <h4 className="hobbyJmbo">Welcome to your new Hubble page.</h4>
-              <hr className="my-2" />
-              <p className="hobbyJmb">This your Home page where you can access your Personality info and manage your Hobby list.</p>
-            </Jumbotron>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Jumbotron>
-              <h1>{this.props.user.personality} is Who you are...</h1>
-              <h4 className="hobbyJmbo">Now let's talk about what you might like to do.</h4>
-              <hr className="my-2" />
-                  <ListGroup>
-                    {this.state.hobbies.map((hobby, index)=>(
-                      <ListGroupItem 
-                      key={index}>{hobby}
-                      </ListGroupItem>
+      <div>
+        <Container>
+          <Row>
+            <Col>
+              <Jumbotron>
+                <h1 className="display-3">Hello, {this.props.user.first_name}</h1>
+                <h4 className="hobbyJmbo">Welcome to your new Hubble page.</h4>
+                <hr className="my-2" />
+                <p className="hobbyJmb">This your Home page where you can access your Personality info and manage your Hobby list.</p>
+              </Jumbotron>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Jumbotron>
+                <h1>{this.props.user.personality} is Who you are...</h1>
+                <h4 className="hobbyJmbo">Now let's talk about what you might like to do.</h4>
+                <hr className="my-2" />
+                <Table>
+                  <thead>
+                    <tr>
+                      <th>Your Hobbies</th>
+                      <th>Meetup Info.</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {this.state.hobbies.map((hobby, index) => (
+                      <tr>
+                        <th
+                          key={index}
+                          name={hobby}>{hobby}
+                        </th>
+                        <th>
+                          <Button className="button" name={hobby} onClick={this.togglemeetUpModal}>Open Info</Button>
+                        </th>
+                      </tr>
                     ))}
-                  </ListGroup>
-            </Jumbotron>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Jumbotron>
-              <Link to={"/survey"}>
-                <Button className="button">Take the Survey Again?</Button>
-              </Link>
-              {/* <Link to={"/profile"}>
+                  </tbody>
+                </Table>
+              </Jumbotron>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Jumbotron>
+                <Link to={"/survey"}>
+                  <Button className="button">Take the Survey Again?</Button>
+                </Link>
+                {/* <Link to={"/profile"}>
                 <Button className="button">Go to Your Page!</Button>
               </Link> */}
-            </Jumbotron>
-          </Col>
-        </Row>
-      </Container>
+              </Jumbotron>
+            </Col>
+          </Row>
+        </Container>
+        <MeatUp
+          modal={this.state.meetupModal}
+          hobbies={this.state.hobbies}
+        />
+      </div>
     );
   }
 }
