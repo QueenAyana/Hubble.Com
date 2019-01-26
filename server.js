a3c4c87e57db31bb22028f6b258784720ca68b1a
Amber <amberrose9167@gmail.com>
2019-01-26 12:31:58 -0800

    test port

--- server.js
+++ server.js
@@ -2,16 +2,15 @@ const routes = require("./routes");
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('./services/passport');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
+//Serve up static assets (usually on heroku)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-//Serve up static assets (usually on heroku)
-
+if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-
+  }
 
 
 app.use(passport.initialize());
